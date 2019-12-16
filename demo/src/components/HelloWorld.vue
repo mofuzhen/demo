6236d39e34ea2bf1b79572fc5fc5c81b07faaa93
mofuzhen <1149060852@qq.com>
2019-12-16 15:49:57 +0800

    第二次提交

--- demo/src/components/HelloWorld.vue
+++ demo/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-radio v-model="radio" label="1">备选项</el-radio>
+    <el-radio v-model="radio" label="1">备选项111111</el-radio>
     <el-radio v-model="radio" label="2">备选项</el-radio>
   </div>
 </template>
